f05bc92b0590353520bc2c025697162ddb65eba9
Athziri pop <athzirimx@hotmail.com>
2022-09-30 12:13:45 -0700

    playlist challenge working with linked lists

---
+++ PlaylistChallenge/src/Album.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private  String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist){
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<Song>();
+    }
+    public boolean addSong(String title, double duration){
+        if(findSong(title) == null){
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title){
+        for(Song song : songs){
+            if(song.getTitle().equals(title)) return song;
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist){
+        if(trackNumber <= 0) return false;
+        if(songs.size() >= trackNumber){
+            Song song = songs.get(trackNumber - 1);
+            playlist.add(song);
+            return true;
+        }
+        System.out.println("Track does not exist");
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playlist){
+        Song song = findSong(title);
+        if(song != null){
+            playlist.add(song);
+            return true;
+        }
+        System.out.println("Song does not exist");
+        return false;
+    }
+
+}
